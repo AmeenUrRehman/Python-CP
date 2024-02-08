e7a9ca66e226e4f05a25fc7570527a147eb081fc
Ameen Ur Rehman <ameenreh2003@gmail.com>
2024-02-08 22:16:31 +0530

    done

---
+++ Hello World.py
@@ -0,0 +1,7 @@
+print("Hello World")
+
+# Example
+num1 = 10
+num2 = 20
+num3 = num1 + num2
+print(num3)
---
+++ Indentation.py
@@ -0,0 +1,14 @@
+# Indentation helps in indicating a block of code
+
+name = "Same"
+if name == "Same":
+    print("Name is Same")
+    ## --> the space before the print is called indentation basically the whitespace.
+else:
+    print("Name is NotSame")
+
+name = "NotSame"
+if name == "Same":
+    print("Name is Same")
+else:
+    print("Name is NotSame")
